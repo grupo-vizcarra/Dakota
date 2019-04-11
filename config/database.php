75bcbcfa05403cc6e5629689c947e93c06f2629d
CarlosHz97 <CarlosHdz_97@outlook.com>
2019-04-11 16:12:58 -0500

    Se ha configurado la conexión a 2 BD's

---
+++ config/database.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    'default' => env('DB_CONNECTION', 'local'),
+    'migrations' => 'migrations',
+    'connections' => [
+        'local' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'pass'),
+            'username' => env('DB_USERNAME', 'user'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+        ],
+
+        'AWS' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_AWS', 'localhost'),
+            'port' => env('DB_PORT_AWS', '3306'),
+            'database' => env('DB_DATABASE_AWS', 'pass'),
+            'username' => env('DB_USERNAME_AWS', 'user'),
+            'password' => env('DB_PASSWORD_AWS', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+        ],
+    ],
+];
