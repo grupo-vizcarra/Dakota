16d3f81b85fea2e30d3a301f3283fae6550f5341
CarlosHz97 <CarlosHdz_97@outlook.com>
2019-05-11 17:00:55 -0500

    Corps para consumir API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -57,6 +57,9 @@ $app->singleton(
 |
 */
 
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+ ]);
 // $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
 // ]);
